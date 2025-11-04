931230380bb6f400a225bc719989ad6f222b0c6f
Shair Khan <w5th.d1tac0de.m2ster.121key@gmail.com>
2025-11-04 13:37:28 +0000

    Add files via upload

---
+++ mymodule2.py
@@ -0,0 +1,21 @@
+
+
+# addition
+def add(a, b):
+    return a + b
+
+# multiplication
+def multiply(a, b):
+    return a * b
+
+# subtraction
+def subtract(a, b):
+    return a - b
+
+# division
+def division(a, b):
+    return a / b
+
+# floor division
+def floor_division(a,b):
+    return a + b
---
+++ mymodule4.py
@@ -0,0 +1,7 @@
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        return f"Hello, my name is {self.name}"
